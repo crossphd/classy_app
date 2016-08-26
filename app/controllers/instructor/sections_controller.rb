7c621dd7073dc9d7dee9f408e789a543ce7f80c4
Chris Ross <chriseross@gmail.com>
2016-08-26 01:46:19 +0000

    now drag and drop reordering available for course sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -2,6 +2,7 @@ class Instructor::SectionsController < ApplicationController
 
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_section, only: [:update]
 
   def new
     @section = Section.new
@@ -12,15 +13,34 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render text: 'updated!'
+  end
+
   private
 
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render text: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
+
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 
   helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:course_id])
+    if params[:course_id].present?
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end
   end
 
   def require_authorized_for_current_course
